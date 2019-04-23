3af3e6e7c0fce4cbf7689cbaa80f284557cd1627
travisbare <travisbare@gmail.com>
2019-04-23 15:12:38 -0500

    added procfile

--- server.js
+++ server.js
@@ -14,9 +14,11 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(__dirname));
+
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
-
+// require("./app/data/friends.js")(app);
 
 
 app.listen(PORT, function () {
